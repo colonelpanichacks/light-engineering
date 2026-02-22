ea230eaf5ec43d7bf8444340fe1ce7a98b845052
Your Name <you@example.com>
2026-02-21 21:04:24 -0500

    Remove oui spy from repo, add to gitignore
    This repo is for light-engineering only.

--- oui spy/rid-test-tx-c5/.pio/build/seeed_xiao_esp32c5/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/seeed_xiao_esp32c5/FrameworkArduino/USB.cpp.o: \
- /Users/colonelpanic/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /Users/colonelpanic/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/colonelpanic/.platformio/packages/framework-arduinoespressif32-libs/esp32c5/include/soc/esp32c5/include/soc/soc_caps.h
